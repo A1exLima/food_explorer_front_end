cc57208c599931d099d2a8c25718fca9cc37e20a
Alex Lima <alex.lima_dev@outlook.com>
2023-10-22 17:59:06 -0300

    <feat>(pages): Layout de apresentação da página Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,26 @@
-import { Container } from "./style"
+import { Container, Main, Presentation } from "./style"
 
 import Header from "../../components/header"
+
+import cookieFruit from "../../assets/images/cookieFruit.png"
 
 export function Home() {
   return (
     <Container>
-      <Header/>
-      
+      <Header />
+
+      <Main>
+
+        <Presentation>
+          <img src={cookieFruit} alt="Biscoito de Frutas" />
+          <div>
+            <h2>Sabores inigualáveis</h2>
+            <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
+          </div>
+        </Presentation>
+
+        
+      </Main>
     </Container>
   )
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,4 +1,59 @@
 import { styled } from "styled-components"
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
 `
+
+export const Main = styled.main`
+  max-width: 112rem;
+  margin: 0 auto;
+`
+
+export const Presentation = styled.figure`
+  position: relative;
+  width: 100%;
+  height: 26rem;
+  margin-top: 16.4rem;
+  padding-right: 10rem;
+  border-radius: 0.8rem;
+  background: ${({ theme }) => theme.COLORS.Gradients200};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: end;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h2 {
+      color: ${({ theme }) => theme.COLORS.Light300};
+      font-family: var(--poppins-font-family);
+      font-size: 4rem;
+      font-weight: 500;
+      line-height: 140%;
+    }
+
+    p {
+      color: ${({ theme }) => theme.COLORS.Light300};
+      font-family: var(--roboto-font-family);
+      font-size: 1.6rem;
+      font-weight: 400;
+      line-height: 100%;
+    }
+  }
+
+  > img {
+    position: absolute;
+    top: -14.6rem;
+    left: -5.4rem;
+    z-index: 1;
+    width: 63.2rem;
+    height: 40.6rem;
+  }
+`
+
+
