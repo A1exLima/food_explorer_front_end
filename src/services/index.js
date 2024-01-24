9b6c7c69e72a484467233785007284496ee51a52
Alex Lima <alex.lima_dev@outlook.com>
2024-01-23 23:36:07 -0300

    <refactor>(home): Alteração BaseURL

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: "http://localhost:3310",
+  baseURL: "https://yearning-junie-devjunior.koyeb.app",
 })
 
 //https://yearning-junie-devjunior.koyeb.app
